52a5dc081f9499dee9c2dfd899b1b5f8dc982531
Samson <samsaf674@gmail.com>
2024-01-22 07:53:48 +0300

    updated

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -20,7 +20,7 @@ function Main() {
   const [user, setUser] = useState<any>({});
   const [profile, setProfile] = useState<any>({});
   useEffect(() => {
-    setUser(auth.authData?.user);
+    setUser(authData?.user);
   }, []);
   useEffect(() => {
     setProfile(user);
