9cabfc6964cea752c33a000888065d5d7e4bbe1d
melba <melba.magiri@gmail.com>
2024-03-22 09:24:23 +0300

    updated

--- src/pages/Auth/create-newPassword.tsx
+++ src/pages/Auth/create-newPassword.tsx
@@ -14,7 +14,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 
 
-//
+//h
 function CreateNewPassword() {
   const [loading, isLoading] = useState(false);
   const [success, setSuccess] = useState(true);
