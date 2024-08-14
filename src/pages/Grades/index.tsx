ca97a452aaf15e3a253d54761ab6c390e82fb6fe
melba <melba.magiri@gmail.com>
2024-08-14 14:08:59 +0300

    changes

--- src/pages/Grades/index.tsx
+++ src/pages/Grades/index.tsx
@@ -246,12 +246,9 @@ function Main() {
                        Name
                     </Table.Th>
                     <Table.Th className="py-0 border-b-0 whitespace-nowrap">
-                       Level
+                       Levels
                     </Table.Th>
-                 
-                    <Table.Th className="py-0 border-b-0 whitespace-nowrap text-center">
-                      Actions
-                    </Table.Th>
+              
                   </Table.Tr>
                 </Table.Thead>
                 <Table.Tbody>
@@ -298,31 +295,7 @@ function Main() {
                             </span>
                           </Table.Td>
                   
-                      <Table.Td className="first:rounded-l-md last:rounded-r-md w-56 bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b] py-3 relative before:block before:w-px before:h-8 before:bg-slate-200 before:absolute before:left-0 before:inset-y-0 before:my-auto before:dark:bg-darkmode-400">
-                        <div className="flex items-center justify-center">
-                          <a
-                            className="flex items-center mr-3 text-success"
-                            href="#"
-                            onClick={() => editRecord(grade)}
-                          >
-                            <Lucide
-                              icon="CheckSquare"
-                              className="w-4 h-4 mr-1"
-                            />{" "}
-                            Edit
-                          </a>
-                          <a
-                            className="flex items-center text-danger"
-                            href="#"
-                            onClick={() => {
-                              setRecordId(grade._id), setConfirmDelete(true);
-                            }}
-                          >
-                            <Lucide icon="Trash2" className="w-4 h-4 mr-1" />{" "}
-                            Delete
-                          </a>
-                        </div>
-                      </Table.Td>
+                  
                     </Table.Tr>
                   ))}
                 </Table.Tbody>
