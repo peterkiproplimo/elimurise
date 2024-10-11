2d446eecbffe853dfe95ac4c90b1a5cc46de6a21
Samson <samsaf674@gmail.com>
2024-10-11 11:16:59 +0300

    updated updated

--- src/pages/GradingLeaningAreas/LearningArea.tsx
+++ src/pages/GradingLeaningAreas/LearningArea.tsx
@@ -27,7 +27,7 @@ const LearningArea: React.FC<LearningAreaProps> = ({
       ...learningArea,
       gradings: updatedGradings,
     };
-    //ss
+    //sss
     setScale({
       ...scale,
       learningAreas: updatedLearningAreas,
