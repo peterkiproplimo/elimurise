0a37ce492725f63f3fa25a396227d10c9a0e5321
Samson <samsaf674@gmail.com>
2024-10-16 11:03:08 +0300

    updated

--- src/pages/SummativeTests/index.tsx
+++ src/pages/SummativeTests/index.tsx
@@ -513,32 +513,35 @@ function Main() {
                             </span>
                           </Table.Td>
                           <Table.Td className="first:rounded-l-md last:rounded-r-md w-56 bg-white border-b-0 dark:bg-darkmode-600 shadow-[20px_3px_20px_#0000000b] py-0 relative before:block before:w-px before:h-8 before:bg-slate-200 before:absolute before:left-0 before:inset-y-0 before:my-auto before:dark:bg-darkmode-400">
-                            <div className="flex items-center justify-center">
-                              <a
-                                className="flex items-center mr-3 text-success"
-                                href="#"
-                                onClick={() => editRecord(test)}
-                              >
-                                <Lucide
-                                  icon="CheckSquare"
-                                  className="w-4 h-4 mr-1 "
-                                />{" "}
-                                Edit
-                              </a>
-                              <a
-                                className="flex items-center text-danger"
-                                href="#"
-                                onClick={() => {
-                                  setRecordId(test._id), setConfirmDelete(true);
-                                }}
-                              >
-                                <Lucide
-                                  icon="Trash2"
-                                  className="w-4 h-4 mr-1"
-                                />{" "}
-                                Delete
-                              </a>
-                            </div>
+                            {test.academicYear && (
+                              <div className="flex items-center justify-center">
+                                <a
+                                  className="flex items-center mr-3 text-success"
+                                  href="#"
+                                  onClick={() => editRecord(test)}
+                                >
+                                  <Lucide
+                                    icon="CheckSquare"
+                                    className="w-4 h-4 mr-1 "
+                                  />{" "}
+                                  Edit
+                                </a>
+                                <a
+                                  className="flex items-center text-danger"
+                                  href="#"
+                                  onClick={() => {
+                                    setRecordId(test._id),
+                                      setConfirmDelete(true);
+                                  }}
+                                >
+                                  <Lucide
+                                    icon="Trash2"
+                                    className="w-4 h-4 mr-1"
+                                  />{" "}
+                                  Delete
+                                </a>
+                              </div>
+                            )}
                           </Table.Td>
                         </Table.Tr>
                       ))}
