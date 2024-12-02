803656356372173b3926eb29ce11793541a1e52f
Samson <samsaf674@gmail.com>
2024-12-02 08:50:47 +0300

    updated

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -331,7 +331,7 @@ function Main() {
                         key={key}
                         className=""
                         onClick={
-                          () => navigate("/home/teachers/" + teacher._id) // Navigate using your custom function
+                          () => navigate("/home/teacher/" + teacher._id) // Navigate using your custom function
                         }
                       >
                         <div className="flex items-center px-5 py-3 mb-3 box zoom-in">
