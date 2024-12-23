265c8c26ef6375fc93dff0a8ab8541cf23073ebc
Samson <samsaf674@gmail.com>
2024-12-23 07:25:34 +0300

    upfsa

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: "esnext", // Ensure modern JavaScript environment
     commonjsOptions: {
       include: ["tailwind.config.js", "node_modules/**"],
     },
