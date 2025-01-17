70fdd1279acb426a55c3b56747a280d0b541b349
Samson <samsaf674@gmail.com>
2025-01-18 02:38:59 +0300

    updated

--- src/pages/Reports/index.tsx
+++ src/pages/Reports/index.tsx
@@ -580,7 +580,7 @@ function Main() {
 
                   <option value={"learner"}>Learner Report</option>
                   <option value="learner-weakness">
-                    Learner Area of Improvement
+                    Learner's Area of Improvement
                   </option>
                   {/* <option value="learner-strength">Learner Strengths</option> */}
 
