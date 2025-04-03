8dbf91686f2a3874fcdc7533a26473ef516d611e
Samson <samsaf674@gmail.com>
2025-04-03 14:52:37 +0300

    updated

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -78,7 +78,7 @@ function Settings() {
       reset({
         ...response.data,
         signatory_role: response?.data?.signatory_role || "Head Teacher", // Default to "Head Teacher" if not present
-        signatory_name: response?.data?.school_head_teacher, // Map existing head teacher name
+        signatory_name: response?.data?.signatory_name, // Map existing head teacher name
         signatory_signature: response?.data?.school_head_teacher_signature, // Map existing signature
         current_term: response?.data?.current_term,
         current_session: response?.data?.current_session,
